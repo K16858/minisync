ca58eee0b8e52dc880d29f137fc09a9fe2a14422
k16858 <ktohda0127@gmail.com>
2026-01-13 10:12:48 +0900

    ファイルエントリ構造体を追加

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,12 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+struct file_entry {
+    char name[256];
+    int  size;
+    int  mtime;
+};
+
 int subst(char *str, char c1, char c2);
 void get_line(char *line, FILE *stream);
 
