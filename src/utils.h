c2c0428bbc84dbd7fdb5b2cedadbb66a62a938bf
k16858 <ktohda0127@gmail.com>
2026-01-22 11:14:37 +0900

    グローバル設定ハンドラを宣言

--- src/utils.h
+++ src/utils.h
@@ -19,6 +19,19 @@ struct msync_config {
     int port;
 };
 
+struct space_entry {
+    char id[64];
+    char name[64];
+    char path[1024];
+    int port;
+};
+
+struct global_config {
+    struct space_entry *spaces;
+    int space_count;
+    int space_capacity;
+};
+
 int subst(char *str, char c1, char c2);
 void get_line(char *line, FILE *stream);
 int get_file_list(char *base_dir, struct file_entry entries[]);
@@ -27,5 +40,9 @@ int create_snapshot(const char *path);
 int load_config(const char *path, struct msync_config *cfg);
 int append_target_json(const char *path, const char *id, const char *name, const char *host, int port, long last_connected_at);
 int load_last_target(const char *path, char *host, size_t host_len, int *port);
+int load_global_config(struct global_config *gcfg);
+int save_global_config(const struct global_config *gcfg);
+int add_space_to_global_config(const char *id, const char *name, const char *path, int port);
+void free_global_config(struct global_config *gcfg);
 
 #endif
